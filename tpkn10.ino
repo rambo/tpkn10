765394bcc34abd69a4e42d3616f6630f70d3c1a2
Eero af Heurlin <eero.afheurlin@iki.fi>
2014-08-01 13:03:03 +0300

    cleanup

--- tpkn10.ino
+++ tpkn10.ino
@@ -37,50 +37,24 @@ inline void select_row(uint8_t row)
 {
     // Drive low
     LATCH_PORT &= (0xff ^ ROW_LATCH);
-    /*
-    Serial.print(F("select_row port value B"));
-    Serial.println(LATCH_PORT, BIN);
-    */
     SPI.transfer(1 << row);
     // Drive high (to latch)
     LATCH_PORT |= ROW_LATCH;
-    /*
-    Serial.print(F("select_row port value B"));
-    Serial.println(LATCH_PORT, BIN);
-    */
 }
 
 inline void select_column_drv(uint8_t columndrv)
 {
-    /*
-    Serial.print(F("select_column_drv called with "));
-    Serial.println(columndrv, DEC);
-    */
-    // Make sure the value is in valid range
-    columndrv &= OE_DECODER_MASK;
     OE_DECODER_PORT &= (0xff ^ OE_DECODER_MASK);
     OE_DECODER_PORT |= (columndrv << OE_DECODER_SHIFT);
-    /*
-    Serial.print(F("select_column_drv port value B"));
-    Serial.println(OE_DECODER_PORT, BIN);
-    */
 }
 
 inline void send_column_data(uint8_t data)
 {
     // Drive low
     LATCH_PORT &= (0xff ^ COLUMN_LATCH);
-    /*
-    Serial.print(F("send_column_data port value B"));
-    Serial.println(LATCH_PORT, BIN);
-    */
     SPI.transfer(data);
     // Drive high (to latch)
     LATCH_PORT |= COLUMN_LATCH;
-    /*
-    Serial.print(F("send_column_data port value B"));
-    Serial.println(LATCH_PORT, BIN);
-    */
 }
 
 void setup()
@@ -95,10 +69,6 @@ uint16_t iteration;
 void loop()
 {
     iteration++;
-    /*
-    Serial.print(F("Iteration "));
-    Serial.println(iteration, DEC);
-    */
     for (uint8_t cdrv=0; cdrv<8; cdrv++)
     {
         select_column_drv(cdrv);
@@ -107,20 +77,11 @@ void loop()
             select_row(row);
             for (uint8_t column=0; column<5; column++)
             {
-                /*
-                Serial.print(F("cdrv="));
-                Serial.println(cdrv, DEC);
-                Serial.print(F("row="));
-                Serial.println(row, DEC);
-                Serial.print(F("column="));
-                Serial.println(column, DEC);
-                */
                 send_column_data(1 << column);
                 // So eyes keep up
-                //delay(10);
+                delay(25);
             }
         }
     }
-    //select_column_drv(0); // This disables all
 }
 
