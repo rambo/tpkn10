04befcb18c414af9a0a4414cd243fc66c6a92d06
Eero af Heurlin <eero.afheurlin@iki.fi>
2014-08-10 15:56:41 +0300

    refactoring and preparing to make the screen refresh interrupt-driven

--- tpkn10.ino
+++ tpkn10.ino
@@ -28,9 +28,6 @@ void init_spi()
     SPI.setDataMode(SPI_MODE0);
     SPI.setBitOrder(MSBFIRST);
     SPI.setClockDivider(SPI_CLOCK_DIV2); 
-    //SPI.setClockDivider(SPI_CLOCK_DIV16); // 1Mhz should still work with messy cables
-    //SPI.setClockDivider(SPI_CLOCK_DIV32); // 500kHz should still work with messy cables
-    //SPI.setClockDivider(SPI_CLOCK_DIV64); // 500kHz should still work with messy cables
 }
 
 void init_bitbang(void)
@@ -84,7 +81,6 @@ void setup()
     Serial.begin(115200);
     init_spi();
     init_bitbang();
-    enable_screen();
     
     // Testpattern
     for (uint8_t row=0; row < ROWS; row++)
@@ -126,6 +122,14 @@ void setup()
     
     dump_framebuffer();
 
+    // Get ready
+    blank_screen();
+    select_row(0);
+    select_column_drv(0);
+    send_column_data(0x0);
+
+    //initTimerCounter2();
+
     Serial.print(F("Booted"));
 }
 
@@ -155,6 +159,104 @@ void dump_framebuffer()
     Serial.println(F("====="));
 }
 
+void initTimerCounter2(void)
+{
+    // Setup for 0.01s delay
+    cli();
+    TCCR2A = 0;                  //stop the timer
+    TCNT2 = 0;                  //zero the timer
+    OCR2A = 200;      //set the compare value (this is about 100uSec on 16MHz/8)
+    TIMSK2 = _BV(OCIE2A);         //interrupt on Compare Match A
+    //start timer, ctc mode, prescaler clk/8
+    TCCR2A = _BV(WGM01);
+    TCCR2B = _BV(CS20) | _BV(CS21);
+    sei();
+}
+
+volatile uint8_t current_row;
+volatile uint8_t current_column_drv;
+const uint8_t coldrvs = (COLUMNS/COLS_PER_DRV);
+
+ISR(TIMER2_COMPA_vect)
+{
+    // Draw one segment
+    blank_screen();
+    select_column_drv(current_column_drv);
+    
+
+    // Scan columns
+    current_column_drv++;
+    if (current_column_drv >= coldrvs)
+    {
+        current_column_drv = 0;
+        current_row++;
+        // And rows
+        if (current_row >= ROWS)
+        {
+            current_row = 0;
+        }
+        blank_screen();
+        select_row(current_row);
+    }
+}
+
+
+inline uint8_t get_column_drv_data(uint8_t coldrv, uint8_t row)
+{
+    uint8_t coldata;
+    switch(coldrv)
+    {
+        case 7:
+        {
+            coldata =  (framebuffer[row][0] & B00011111);
+            break;
+        }
+        case 6:
+        {
+            coldata =  (framebuffer[row][0] & B11100000) >> 5;
+            coldata |= (framebuffer[row][1] & B00000011) << 3;
+            break;
+        }
+        case 5:
+        {
+          
+            coldata =  (framebuffer[row][1] & B01111100) >> 2; 
+            break;
+        }
+        case 4:
+        {
+            coldata =  (framebuffer[row][1] & B10000000) >> 7;
+            coldata |= (framebuffer[row][2] & B00001111) << 1;
+            break;
+        }
+        case 3:
+        {
+            coldata =  (framebuffer[row][2] & B11110000) >> 4;
+            coldata |= (framebuffer[row][3] & B00000001) << 4;
+            break;
+        }
+        case 2:
+        {
+            coldata =  (framebuffer[row][3] & B00111110) >> 1;
+            break;
+        }
+        case 1:
+        {
+            coldata =  (framebuffer[row][3] & B11000000) >> 6;
+            coldata |= (framebuffer[row][4] & B00000111) << 2;
+            break;
+        }
+        case 0:
+        {
+            coldata =  (framebuffer[row][4] & B11111000) >> 3;
+            break;
+        }
+    }
+    return coldata;
+}
+
+
+
 unsigned long last_move_time;
 uint8_t coldata;
 uint8_t startbit;
@@ -171,55 +273,7 @@ void loop()
         {
             blank_screen();
             select_column_drv(coldrv);
-            switch(coldrv)
-            {
-                case 7:
-                {
-                    coldata =  (framebuffer[row][0] & B00011111);
-                    break;
-                }
-                case 6:
-                {
-                    coldata =  (framebuffer[row][0] & B11100000) >> 5;
-                    coldata |= (framebuffer[row][1] & B00000011) << 3;
-                    break;
-                }
-                case 5:
-                {
-                  
-                    coldata =  (framebuffer[row][1] & B01111100) >> 2; 
-                    break;
-                }
-                case 4:
-                {
-                    coldata =  (framebuffer[row][1] & B10000000) >> 7;
-                    coldata |= (framebuffer[row][2] & B00001111) << 1;
-                    break;
-                }
-                case 3:
-                {
-                    coldata =  (framebuffer[row][2] & B11110000) >> 4;
-                    coldata |= (framebuffer[row][3] & B00000001) << 4;
-                    break;
-                }
-                case 2:
-                {
-                    coldata =  (framebuffer[row][3] & B00111110) >> 1;
-                    break;
-                }
-                case 1:
-                {
-                    coldata =  (framebuffer[row][3] & B11000000) >> 6;
-                    coldata |= (framebuffer[row][4] & B00000111) << 2;
-                    break;
-                }
-                case 0:
-                {
-                    coldata =  (framebuffer[row][4] & B11111000) >> 3;
-                    break;
-                }
-            }
-            send_column_data(coldata);
+            send_column_data(get_column_drv_data(coldrv, row));
             enable_screen();
             delayMicroseconds(200);
         }
